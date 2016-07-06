9e08ba1cabdce0128c17c2790f79fe400728bf14
Abiyaruth joseph <abiyaruth@gmail.com>
2016-07-05 23:00:41 -0400

    added autoincrement

--- src/JavaScript/script.js
+++ src/JavaScript/script.js
@@ -3,13 +3,35 @@ var modifier = 1
 function pointCounter() {
   counter = counter + modifier
 $('h1').html('Points:' + counter.toFixed(2))
+switch(parseFloat(counter.toFixed(2))){
+  case 10.00 : $("#counter").removeAttr("onclick");
+              $("#modify").attr({"onclick":"mod(0.1)",
+                                  "class":"buttonShape"})
+              $("#auto").attr("onclick","auto()")
+              break;
+  case 16.00 :$("#counter").removeAttr("onclick");
+              $("#modify").attr("onclick","mod(0.01)")
+              break;
 }
-function modify() {
-    // alert('Click on modifier or autoclick');
-  if (counter === 10) {
-    mod(0.1)
-  }
 }
+// function modify() {
+//   alert('Click on modifier or autoclick');
+//   if (counter === 10) {
+//     mod(0.1)
+//   }
+// }
 function mod(x) {
   modifier = x
+  $("#counter").attr("onclick","pointCounter()");
+  $("#modify").removeAttr("onclick")
+  $("#auto").removeAttr("onclick")
+}
+function auto() {
+for(var i=0;i<5;i++)
+{
+  pointCounter();
+}
+  $("#auto").removeAttr("onclick")
+  $("#counter").attr("onclick","pointCounter()");
+  $("#modify").removeAttr("onclick")
 }
