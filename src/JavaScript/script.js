7e700ca2d769687a16bdac24e8e45884ef60e748
Abiyaruth joseph <abiyaruth@gmail.com>
2016-07-05 22:15:36 -0400

    implemented modifier

--- src/JavaScript/script.js
+++ src/JavaScript/script.js
@@ -1,5 +1,15 @@
-var counter = 0;
+var counter = 0
+var modifier = 1
 function pointCounter() {
-  counter++;
-  $('h1').html('Points:'+counter);
+  counter = counter + modifier
+$('h1').html('Points:' + counter.toFixed(2))
+}
+function modify() {
+    // alert('Click on modifier or autoclick');
+  if (counter === 10) {
+    mod(0.1)
+  }
+}
+function mod(x) {
+  modifier = x
 }
